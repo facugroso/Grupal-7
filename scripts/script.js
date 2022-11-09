8bf75d913ca0174f585fdb66b5a224563d3dab63
Romina Calleja <romycalleja.12.08@gmail.com>
2022-11-09 20:08:59 -0300

    comit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 const boton_buscar = document.getElementById("btnGet1")
 const API_URL = "https://636c18037f47ef51e142259b.mockapi.io/";
 const BOTON_CREAR = document.getElementById("btnPost")
+const BOTON_MODIFICAR= document.getElementById("btnPut")
+const modal= document.getElementById("dataModal")
+const BOTON_BORRAR= document.getElementById("btnDelete")
+let userarray = []
 
 
 const listData = () => {
@@ -14,13 +18,33 @@ const listData = () => {
 listData();
 
 boton_buscar.addEventListener("click", () => {
-    fetch(API_URL, {
+    fetch(API_URL+ "users/" + document.getElementById("inputGet1Id").value , {
       method: "GET",
       headers: { "Content-Type": "application/json" },
+      
+     })
+    
+      .then((response) => response.json())
+      .then((dataResponse) => {
+        console.log(dataResponse);
+        userarray=dataResponse
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Ocurrió un error");
+      });
+      showusers()
+  });
+
+  BOTON_CREAR.addEventListener("click", () => {
+    fetch("https://636c18037f47ef51e142259b.mockapi.io/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        name: "name",
-        lastname: "lastname",
-        id:""
+        name: document.getElementById("inputPostNombre").value,
+        lastname:document.getElementById("inputPostApellido").value,
+        
+        
      })
     })
       .then((response) => response.json())
@@ -33,14 +57,15 @@ boton_buscar.addEventListener("click", () => {
       });
   });
 
-  boton_buscar.addEventListener("click", () => {
-    fetch("https://636c18037f47ef51e142259b.mockapi.io/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+  BOTON_MODIFICAR.addEventListener("click",()=> {
+fetch(API_URL+ "users/" + document.getElementById("inputPutId").value , {
+  method: "PUT",
+  headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        name: "Name",
-        lastname: "Lastname",
-        id:""
+        name: document.getElementById("inputPostNombre").value,
+        lastname:document.getElementById("inputPostApellido").value,
+        
+        
      })
     })
       .then((response) => response.json())
@@ -52,6 +77,46 @@ boton_buscar.addEventListener("click", () => {
         alert("Ocurrió un error");
       });
   });
+
+  BOTON_BORRAR.addEventListener("click",()=> {
+    fetch(API_URL+ "users/" + document.getElementById("inputDelete").value , {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name: document.getElementById("inputPostNombre").value,
+            lastname:document.getElementById("inputPostApellido").value,
+            
+            
+         })
+        })
+          .then((response) => response.json())
+          .then((dataResponse) => {
+            console.log(dataResponse);
+          })
+          .catch((error) => {
+            console.log(error);
+            alert("Ocurrió un error");
+          });
+      });
+    
+
+  function showusers(){
+    console.log(userarray)
+    let userlist = "";
+
+  for (let i = 0; i < userarray.length; i++) {
+    let users = userarray[i];
+    {
+      userlist += `
+      <div class="col-md-6">
+                <ul class="border bg-dark text-white my-3 list-group list-group-flush" id="results">
+                ${users.id} ${users.name} ${users.lastname} </ul>
+            </div>
+        
+`;
+      document.getElementById("results").innerHTML = userlist;
+
+  }}}
 
  
  
