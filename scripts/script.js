befceb8157410227be35cd28956a25db91d6424b
christiansanguinetti <christiansanguinetti882@gmail.com>
2022-11-10 20:36:21 -0300

    termiando

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ const modal = document.getElementById("dataModal")
 const BOTON_BORRAR = document.getElementById("btnDelete")
 let userarray = []
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
   fetch(API_URL + "users/" + document.getElementById("inputGet1Id").value, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
@@ -14,7 +14,6 @@ document.addEventListener("DOMContentLoaded", ()=> {
   })
     .then((response) => response.json())
     .then((dataResponse) => {
-      console.log(dataResponse);
       userarray = dataResponse
     })
     .catch((error) => {
@@ -31,10 +30,9 @@ boton_buscar.addEventListener("click", () => {
   })
     .then((response) => response.json())
     .then((dataResponse) => {
-      console.log(dataResponse);
       userarray = dataResponse
       if (Array.isArray(dataResponse)) showusers(dataResponse)
-      else{showusers2(dataResponse)}
+      else { showusers2(dataResponse) }
     })
     .catch((error) => {
       console.log(error);
@@ -52,12 +50,17 @@ BOTON_CREAR.addEventListener("click", () => {
 
 
     })
+  });
+  fetch(API_URL + "users/", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+
   })
     .then((response) => response.json())
     .then((dataResponse) => {
-      console.log(dataResponse);
-      showusers(userarray)
-      showusers2(dataResponse)
+      userarray = dataResponse
+      if (Array.isArray(dataResponse)) showusers(dataResponse)
+      else { showusers2(dataResponse) }
     })
     .catch((error) => {
       console.log(error);
@@ -65,29 +68,70 @@ BOTON_CREAR.addEventListener("click", () => {
     });
 });
 
-BOTON_MODIFICAR.addEventListener("click", () => {
-  fetch(API_URL + "users/" + document.getElementById("inputPutId").value, {
+BOTON_MODIFICAR.addEventListener("click", async () => {
+  let user = {}
+  await fetch(API_URL + "users/" + document.getElementById("inputPutId").value, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+
+  })
+    .then((response) => response.json())
+    .then((dataResponse) => {
+      console.log(dataResponse);
+      user = dataResponse;
+
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Ocurrió un error");
+    });
+  const nombre = document.getElementById("inputPutNombre");
+  const apellido = document.getElementById("inputPutApellido");
+  console.log(user)
+  nombre.value = user.name;
+  apellido.value = user.lastname;
+});
+
+
+const guardar = document.getElementById("guardar_cambio")
+guardar.addEventListener("click", async () => {
+  await fetch(API_URL + "users/" + document.getElementById("inputPutId").value, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      name: document.getElementById("inputPostNombre").value,
-      lastname: document.getElementById("inputPostApellido").value,
+      name: document.getElementById("inputPutNombre").value,
+      lastname: document.getElementById("inputPutApellido").value,
 
 
     })
   })
+    .catch((error) => {
+      console.log(error);
+      alert("Ocurrió un error");
+    });
+  await fetch(API_URL + "users/", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+
+  })
     .then((response) => response.json())
     .then((dataResponse) => {
-      console.log(dataResponse);
+      showusers(dataResponse)
     })
     .catch((error) => {
       console.log(error);
       alert("Ocurrió un error");
     });
-});
+})
 
-BOTON_BORRAR.addEventListener("click", () => {
-  fetch(API_URL + "users/" + document.getElementById("inputDelete").value, {
+
+
+
+
+
+
+BOTON_BORRAR.addEventListener("click", async () => {
+  await fetch(API_URL + "users/" + document.getElementById("inputDelete").value, {
     method: "DELETE",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -105,29 +149,41 @@ BOTON_BORRAR.addEventListener("click", () => {
       console.log(error);
       alert("Ocurrió un error");
     });
+  await fetch(API_URL + "users/", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+
+  })
+    .then((response) => response.json())
+    .then((dataResponse) => {
+      showusers(dataResponse)
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("Ocurrió un error");
+    });
 });
 
 
 function showusers(userarray) {
-  console.log(userarray)
   let userlist = "";
 
 
   for (let i = 0; i < userarray.length; i++) {
     let users = userarray[i];
     userlist += `
-      <div class="col-md-6">
-      <ul>
-                ${users.id} ${users.name} ${users.lastname} </ul>
-            </div>
-        
-`;
+    <div class="col-md-6">
+    <ul>
+    ${users.id} ${users.name} ${users.lastname} </ul>
+    </div>
+    
+    `;
 
   }
   document.getElementById("results").innerHTML = userlist;
 }
 
-function showusers2 (userarray){
-  let users= userarray
+function showusers2(userarray) {
+  let users = userarray
   document.getElementById("results").innerHTML = `${users.id} ${users.name} ${users.lastname}`;
 }
